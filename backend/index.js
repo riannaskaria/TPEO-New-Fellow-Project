9693571d7bcb76d555ac25798c870019d6f010a5
Rianna Skaria <rianna.skaria@gmail.com>
2025-02-20 18:54:44 -0600

    updated port

--- backend/index.js
+++ backend/index.js
@@ -9,5 +9,5 @@ app.get("/", (req, res) => {
   res.send("Hello from the backend!");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
