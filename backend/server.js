eb176ddde561527c00c3025c00d8357bf506f436
Hantao <hantaojin@gmail.com>
2025-03-10 15:29:17 -0500

    Changed cors middleware options

--- backend/server.js
+++ backend/server.js
@@ -14,15 +14,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;	
 
 // Configure middleware
-// app.use(
-// 	cors({
-// 		origin: "http://localhost:3000", // Frontend URL
-// 		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-// 		allowedHeaders: ['Content-Type', 'Authorization'],
-// 		credentials: true
-// 	})
-// );
-app.use(cors());
+app.use(
+	cors({
+		origin: "http://localhost:3000", // Frontend URL
+		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+		allowedHeaders: ['Content-Type', 'Authorization'],
+		credentials: true
+	})
+);
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
